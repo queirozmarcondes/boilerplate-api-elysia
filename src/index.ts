2f7aaa0d943017da8d3e4f60af3f019ba88d25df
queirozmarcondes <renatodequeirozmarcondes@gmail.com>
2025-06-26 23:29:06 -0400

    current status and server timestamp.

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,24 @@ import { swaggerPlugin } from './plugins/swagger.pluggin';
 
  new Elysia()
   .use(swaggerPlugin())
+   .get(
+    '/status',
+    () => ({
+      status: '🟢 Online',
+      timestamp: new Date().toISOString(),
+    }),
+    {
+      detail: {
+        summary: 'Server status check',
+        description: 'Returns current status and server timestamp.',
+        tags: ['Status'],
+      },
+      response: t.Object({
+        status: t.String(),
+        timestamp: t.String({ format: 'date-time' }),
+      }),
+    }
+  )
   .use(user)
   .listen(3000)
 
